3d1279a770f30bde53762027e56e0f542998d084
Murali Singh <68021313+muraliSingh7@users.noreply.github.com>
2025-04-02 02:02:35 +0530

    Font optimization (#3)
    * Revamping UI
    
    * Fixing styling
    
    * Updating styling
    
    * Changing images and adding animations
    
    * Added typewritter animation fix button redirect at fewplaces
    
    * Fixing UI issues
    
    * UI Fix
    
    * Updating z-index
    
    * Font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-
+import { Cute_Font, Fira_Code, Inter } from "next/font/google";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
@@ -11,6 +9,12 @@ export const metadata = {
   description: "Portfolio Website of Murali Singh created with Next app",
 };
 
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-fira-code",
+});
+
 export default function RootLayout({
   children,
 }: {
@@ -18,19 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cute+Font&family=Fira+Code:wght@300..700&family=Inter:ital,opsz,wght@0,14..32,100;0,14..32,500&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={inter.className}>{children}</body>
+      <head/>
+      <body className={`${firaCode.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
